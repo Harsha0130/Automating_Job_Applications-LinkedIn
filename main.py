0f6dee24ba29ea4a2465d2ce275b51b8c04030b6
Harsha <harshahm0130@gmail.com>
2024-07-23 18:34:28 +0530

    Navigating through the account setup and heading straight to the filtered Jobs section.

---
+++ main.py
@@ -0,0 +1,27 @@
+from selenium import webdriver
+from selenium.webdriver.common.by import By
+# from selenium.webdriver.common.keys import Keys
+import os
+
+EMAIl = os.environ["Email"]
+PASSWORD = os.environ["Password"]
+
+chrome_options = webdriver.ChromeOptions()
+chrome_options.add_experimental_option("detach", True)
+
+driver = webdriver.Chrome(options=chrome_options)
+driver.get("https://www.linkedin.com/jobs/search/?currentJobId=3962661047&f_"
+           "AL=true&geoId=105214831&keywords=python%20developer&location=Bengaluru"
+           "%2C%20Karnataka%2C%20India&origin=JOB_SEARCH_PAGE_JOB_FILTER&refresh=true")
+
+signin = driver.find_element(By.CLASS_NAME, value="btn-secondary-emphasis")
+signin.click()
+
+email = driver.find_element(By.ID, value="username")
+email.send_keys(EMAIl)
+
+password = driver.find_element(By.ID, value="password")
+password.send_keys(PASSWORD)
+
+enter = driver.find_element(By.CLASS_NAME, value="from__button--floating")
+enter.click()
